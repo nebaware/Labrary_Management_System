ea265c329bb64cd6f8046b316d7feb461ca21f58
Muler8905 <mulukenugamo7@gmail.com>
2025-06-05 06:49:48 -0700

    version 2

--- src/model/Author.java
+++ src/model/Author.java
@@ -3,16 +3,19 @@ package model;
 public class Author {
     private String name;
     private String biography;
-
-    public Author(String name, String biography) {
+ private String position;
+    public Author(String name, String biography String position) {
         this.name = name;
         this.biography = biography;
+        this.position =position;
     }
 
     public String getName() {
         return name;
     }
-
+public String getPosion() {
+        return position;
+    }
     public String getBiography() {
         return biography;
     }
